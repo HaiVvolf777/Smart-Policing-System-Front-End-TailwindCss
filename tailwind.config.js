875f6ec8591925291fdd2529362e2c7790201fe7
Haider-star <73600973+Haider-star@users.noreply.github.com>
2022-08-30 06:01:24 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'pop': ['Poppins'],
+     },
+     colors: {
+      'black-1': '#171717',
+      'dark-1': '#406882',
+      'light-1': '#B1D0E0',
+      'botn': '#17a2b8'
+    
+    },
+    },
+  },
+  plugins: [],
+  
+}
